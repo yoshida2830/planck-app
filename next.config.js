ad0cdecb6ded4cda4bb54a35be0575d5c7a0403c
yoshida2830 <64234706+yoshida2830@users.noreply.github.com>
2025-07-02 00:15:53 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
+  }
+}
 
+module.exports = nextConfig
